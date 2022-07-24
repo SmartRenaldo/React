f8679a44bada6dd87a8ef88e2d2d8db861c0458d
SmartRenaldo <correctgavin@gmail.com>
2022-07-24 21:38:36 +0930

    nested components

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -4,20 +4,21 @@ import ReactDom from 'react-dom';
 function Greeting() {
   return (
     <React.Fragment>
-      <h2>JSX Rules</h2>
-      <article>This is Renaldo and this is my first component!</article>
+      <TopMsg />
+      <Animal />
       <input type='text' />
     </React.Fragment>
   );
 }
 
-// messy way, not recommended
-// const Greeting = () => {
-//   return React.createElement(
-//     'div',
-//     {},
-//     React.createElement('h1', {}, 'hello Renaldo')
-//   );
-// };
+const Animal = () => <h1>alexander King</h1>;
+const TopMsg = () => {
+  return (
+    <div>
+      <h2>JSX Rules</h2>
+      <article>This is Renaldo and this is my first component!</article>
+    </div>
+  );
+};
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
