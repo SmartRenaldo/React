6c862f785472be7feace42e6ef664cd07b3da0b6
SmartRenaldo <correctgavin@gmail.com>
2022-07-24 22:54:18 +0930

    integrate bootstrap

--- best-selling-books/src/index.js
+++ best-selling-books/src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Greeting() {
+  return (
+    <React.Fragment>
+      <Button variant='primary'>Button as link</Button>
+      <Button variant='success'>Button as link</Button>
+    </React.Fragment>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<Greeting />, document.getElementById('root'));
