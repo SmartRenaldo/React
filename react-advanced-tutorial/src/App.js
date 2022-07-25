dab2656aeef0e1493bbfdb3a28183efab79054c9
SmartRenaldo <correctgavin@gmail.com>
2022-07-25 21:16:38 +0930

    react advanced tutorial useState

--- react-advanced-tutorial/src/App.js
+++ react-advanced-tutorial/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Setup from './tutorial/1-useState/setup/1-error-example';
+import Final from './tutorial/1-useState/final/1-error-example';
+
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <Setup />
+      <Final />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
