c20f180213b9928a6f0752d68bfb083790574b89
SmartRenaldo <correctgavin@gmail.com>
2022-07-25 14:23:37 +0930

    best selling books props

--- best-selling-books/src/index.js
+++ best-selling-books/src/index.js
@@ -3,28 +3,44 @@ import ReactDom from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
+const firstBook = {
+  img: 'https://images-fe.ssl-images-amazon.com/images/I/71FXfPs-iyL._AC_UL604_SR604,400_.jpg',
+  title: 'Where the Crawdads Sing',
+  author: 'Delia Owens',
+};
+
+const secondBook = {
+  img: 'https://images-fe.ssl-images-amazon.com/images/I/71luEOJVzJL._AC_UL604_SR604,400_.jpg',
+  title: 'The Lord Of The Rings [Illustrated Edition]',
+  author: 'J r r Tolkien',
+};
+
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const author = 'Delia Owens';
-const Book = () => {
-  const title = 'Where the Crawdads Sing';
+const Book = (props) => {
+  console.log(props);
 
   return (
     <article className='book'>
-      <img src='https://images-fe.ssl-images-amazon.com/images/I/71FXfPs-iyL._AC_UL604_SR604,400_.jpg' />
-      <h1 className='display-5'>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
+      <img src={props.img} />
+      <h1 className='display-6'>{props.title}</h1>
+      <h4>{props.author}</h4>
+      <p>{props.job}</p>
     </article>
   );
 };
