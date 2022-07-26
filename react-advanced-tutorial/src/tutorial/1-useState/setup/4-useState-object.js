52b93efa079a88b0725c5c947417056b753bc5f8
SmartRenaldo <correctgavin@gmail.com>
2022-07-26 15:06:01 +0930

    react advanced tutorial useState multiple state values

--- react-advanced-tutorial/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced-tutorial/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,25 +1,23 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  const [person, setPerson] = useState({
-    name: 'alexanda',
-    age: 27,
-    message: 'hello world!',
-  });
+  const [name, setName] = useState('alexanda');
+  const [age, setAge] = useState(27);
+  const [message, setMessage] = useState('hello world!');
 
   const changeMessage = () => {
-    if (person.message === 'hello world!') {
-      setPerson({ ...person, message: 'random message' });
+    if (message === 'hello world!') {
+      setMessage('random message');
     } else {
-      setPerson({ ...person, message: 'hello world!' });
+      setMessage('hello world!');
     }
   };
 
   return (
     <>
-      <h3 className='lead fs-2 fw-bolder fst-italic'>{person.name}</h3>
-      <h3 className='lead fs-2 fw-bolder fst-italic'>{person.age}</h3>
-      <h3 className='lead fs-2 fw-bolder fst-italic'>{person.message}</h3>
+      <h3 className='lead fs-2 fw-bolder fst-italic'>{name}</h3>
+      <h3 className='lead fs-2 fw-bolder fst-italic'>{age}</h3>
+      <h3 className='lead fs-2 fw-bolder fst-italic'>{message}</h3>
       <button className='btn btn-info' onClick={changeMessage}>
         Change Message
       </button>
