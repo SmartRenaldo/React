40be28011c9bfaf9fc4d8fb272b1f99612bd21c2
SmartRenaldo <correctgavin@gmail.com>
2022-07-26 14:59:17 +0930

    react advanced tutorial useState object

--- react-advanced-tutorial/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced-tutorial/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,30 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'alexanda',
+    age: 27,
+    message: 'hello world!',
+  });
+
+  const changeMessage = () => {
+    if (person.message === 'hello world!') {
+      setPerson({ ...person, message: 'random message' });
+    } else {
+      setPerson({ ...person, message: 'hello world!' });
+    }
+  };
+
+  return (
+    <>
+      <h3 className='lead fs-2 fw-bolder fst-italic'>{person.name}</h3>
+      <h3 className='lead fs-2 fw-bolder fst-italic'>{person.age}</h3>
+      <h3 className='lead fs-2 fw-bolder fst-italic'>{person.message}</h3>
+      <button className='btn btn-info' onClick={changeMessage}>
+        Change Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
