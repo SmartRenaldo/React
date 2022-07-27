804276f2757b8d29af1bd6e2da42e0d91f592fcf
SmartRenaldo <correctgavin@gmail.com>
2022-07-27 12:42:17 +0930

    react advanced tutorial multiple returns fetching

--- react-advanced-tutorial/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ react-advanced-tutorial/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,13 +1,42 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState('default user');
 
-  if (loading) {
-    return <h2>Loading...</h2>;
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((user) => {
+        const { name } = user;
+        setUser(name);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    );
   }
 
-  return <h2>multiple returns</h2>;
+  if (isError) {
+    return (
+      <div>
+        <h2>Error...</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2>{user}</h2>
+    </div>
+  );
 };
 
 export default MultipleReturns;
