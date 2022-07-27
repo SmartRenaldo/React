a209c9df6b28107ac18fd86422f5c5f480f4ef0a
SmartRenaldo <correctgavin@gmail.com>
2022-07-27 14:57:52 +0930

    react advanced tutorial ternary operator

--- react-advanced-tutorial/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-tutorial/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 const ShortCircuit = () => {
   const [text, setText] = useState('');
   const [text2, setText2] = useState('X');
+  const [isError, setIsError] = useState(false);
   const firstValueText1 = text || 'hello world';
   const secondValueText1 = text && 'hello world';
   const firstValueText2 = text2 || 'hello world';
@@ -36,6 +37,14 @@ const ShortCircuit = () => {
       <div className='display-6 my-4'>
         {text2 && <p className='text-success'>dora wax</p>}
       </div>
+      {/* <button className='btn' onClick={() => setIsError(!isError)}>
+        toggle error
+      </button> */}
+      <button className='btn' onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {isError && <h1 className='display-6 my-4'>Error...</h1>}
+      {/* {isError && <h1 className='display-6 my-4'>Error...</h1>} */}
     </>
   );
 };
