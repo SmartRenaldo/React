61829c736ff1afa5780c64266fa7adc3595a68e3
SmartRenaldo <correctgavin@gmail.com>
2022-07-28 20:38:51 +0930

    react advanced tutorial multiple inputs

--- react-advanced-tutorial/src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ react-advanced-tutorial/src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -10,10 +10,20 @@ const ControlledInputs = () => {
   const [person, setPerson] = useState({ firstName: '', email: '', age: '' });
   const [people, setPeople] = useState([]);
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setPerson({ ...person, [name]: value });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (person.firstName && person.email && person.age) {
+      const newPerson = { ...person, id: new Date().getTime().toString() };
+      setPeople([...people, newPerson]);
+      setPerson({ firstName: '', email: '', age: '' });
+    }
   };
 
   return (
@@ -55,11 +65,12 @@ const ControlledInputs = () => {
           </button>
         </form>
         {people.map((person, index) => {
-          const { id, firstName, email } = person;
+          const { id, firstName, email, age } = person;
           return (
             <div className='item' key={id}>
-              <h4>{firstName}</h4>
-              <p>{email}</p>
+              <h4 className='btn btn-info'>{firstName}</h4>
+              <h4 className='btn btn-info'>{email}</h4>
+              <h4 className='btn btn-info'>{age}</h4>
             </div>
           );
         })}
