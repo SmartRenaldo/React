6104b27a106a8709726d6303697b035f53bdfcd1
SmartRenaldo <correctgavin@gmail.com>
2022-08-05 00:12:33 +0930

    swiper

--- swiper/src/App.js
+++ swiper/src/App.js
@@ -1,31 +1,38 @@
-import './App.css';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-
 import React, { Component } from 'react';
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/swiper-bundle.min.css';
+import './App.css';
+Swiper.use([Navigation, Pagination]);
 
 export default class App extends Component {
   state = {
     list: ['1111', '2222', '3333', '4444', '5555'],
   };
+
+  componentDidMount() {
+    new Swiper('.swiper', {
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+  }
+
   render() {
     return (
       <div>
-        <div class='swiper'>
-          <div class='swiper-wrapper'>
-            <div class='swiper-slide'>Slide 1</div>
-            <div class='swiper-slide'>Slide 2</div>
-            <div class='swiper-slide'>Slide 3</div>
+        <div
+          className='swiper'
+          style={{ height: '200px', background: 'yellow' }}
+        >
+          <div className='swiper-wrapper'>
+            {this.state.list.map((item) => (
+              <div className='swiper-slide' key={item}>
+                {item}
+              </div>
+            ))}
           </div>
-          <div class='swiper-pagination'></div>
 
-          <div class='swiper-button-prev'></div>
-          <div class='swiper-button-next'></div>
-
-          <div class='swiper-scrollbar'></div>
+          <div className='swiper-pagination'></div>
         </div>
       </div>
     );
