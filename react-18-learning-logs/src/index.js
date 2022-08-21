cc9bfd07bd3758543649acbc704e06cc67004125
SmartRenaldo <correctgavin@gmail.com>
2022-08-21 22:32:09 +0930

    react 18 learning log

--- react-18-learning-logs/src/index.js
+++ react-18-learning-logs/src/index.js
@@ -6,6 +6,36 @@ const App = (
     <div className='item'>
       <div className='date'>
         <div className='month'>February</div>
+        <div className='day'>25</div>
+      </div>
+      <div className='content'>
+        <div className='title'>Learn Node.js</div>
+        <div className='time'>30 min</div>
+      </div>
+    </div>
+    <div className='item'>
+      <div className='date'>
+        <div className='month'>February</div>
+        <div className='day'>28</div>
+      </div>
+      <div className='content'>
+        <div className='title'>Learn Angular</div>
+        <div className='time'>25 min</div>
+      </div>
+    </div>
+    <div className='item'>
+      <div className='date'>
+        <div className='month'>March</div>
+        <div className='day'>05</div>
+      </div>
+      <div className='content'>
+        <div className='title'>Learn Vue</div>
+        <div className='time'>30 min</div>
+      </div>
+    </div>
+    <div className='item'>
+      <div className='date'>
+        <div className='month'>February</div>
         <div className='day'>22</div>
       </div>
       <div className='content'>
