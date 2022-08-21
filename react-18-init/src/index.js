e8a7355ef74083de05397eb894dcd7c76e583633
SmartRenaldo <correctgavin@gmail.com>
2022-08-21 20:15:14 +0930

    react 18 init

---
+++ react-18-init/src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+
+const App = (
+  <div>
+    <h1>This is a React 18 project</h1>
+    <p>Hello React 18!</p>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(App);
