1bcbd821c2de63b7894ccbaaae003dc11e90019b
SmartRenaldo <correctgavin@gmail.com>
2022-08-22 22:02:25 +0930

    react 18 class component

---
+++ react-18-class-component/src/App.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Class Component</h2>
+        <div>Hello App!</div>
+      </div>
+    );
+  }
+}
+
+export default App;
