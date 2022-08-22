095da017eac2328413315db3c7f3b7d970ee0501
SmartRenaldo <correctgavin@gmail.com>
2022-08-22 21:37:10 +0930

    react 18 component

--- react-18-component/src/index.js
+++ react-18-component/src/index.js
@@ -1,50 +1,8 @@
 import ReactDOM from 'react-dom/client';
-import './index.css';
 
-const App = (
-  <div className='logs'>
-    <div className='item'>
-      <div className='date'>
-        <div className='month'>February</div>
-        <div className='day'>22</div>
-      </div>
-      <div className='content'>
-        <div className='title'>Learn React</div>
-        <div className='time'>40 min</div>
-      </div>
-    </div>
-    <div className='item'>
-      <div className='date'>
-        <div className='month'>February</div>
-        <div className='day'>25</div>
-      </div>
-      <div className='content'>
-        <div className='title'>Learn Node.js</div>
-        <div className='time'>30 min</div>
-      </div>
-    </div>
-    <div className='item'>
-      <div className='date'>
-        <div className='month'>February</div>
-        <div className='day'>28</div>
-      </div>
-      <div className='content'>
-        <div className='title'>Learn Angular</div>
-        <div className='time'>25 min</div>
-      </div>
-    </div>
-    <div className='item'>
-      <div className='date'>
-        <div className='month'>March</div>
-        <div className='day'>05</div>
-      </div>
-      <div className='content'>
-        <div className='title'>Learn Vue</div>
-        <div className='time'>30 min</div>
-      </div>
-    </div>
-  </div>
-);
+function App() {
+  return <div>Hello World!</div>;
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(App);
+const root = ReactDOM.createROOT(document.getElementById('root'));
+root.render(<App />);
