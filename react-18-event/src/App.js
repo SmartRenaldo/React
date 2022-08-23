a14865c1d959f50d3b47ddcaa31498e338f3aa9c
SmartRenaldo <correctgavin@gmail.com>
2022-08-23 22:29:18 +0930

    event

--- react-18-event/src/App.js
+++ react-18-event/src/App.js
@@ -1,14 +1,44 @@
-import { Component } from 'react';
+const App = () => {
+  const clickHandler = () => {
+    // In React, you can not cancel default behaviors by return false
+    alert('Awesome!');
+  };
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Class Component</h2>
-        <div>Hello App!</div>
-      </div>
-    );
-  }
-}
+  const clickHandlerCancelDefaultBehavior = (event) => {
+    // In React, you can not cancel default behaviors by return false
+    // preventDefault() could be used to cancel default behaviors
+    event.preventDefault();
+    event.stopPropagation();
+    alert('Awesome!');
+  };
+  return (
+    <div
+      onClick={() => {
+        alert('parent alert!');
+      }}
+      style={{ width: 200, height: 200, margin: '100px auto' }}
+    >
+      <button onClick={clickHandler}>Awesome!</button>
+      <button
+        onClick={() => {
+          alert('Legendary!');
+        }}
+      >
+        Lenegdary!
+      </button>
+      <br />
+      <a href='https://www.google.com' onClick={clickHandler}>
+        To Google
+      </a>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <a
+        href='https://www.google.com'
+        onClick={clickHandlerCancelDefaultBehavior}
+      >
+        To Google
+      </a>
+    </div>
+  );
+};
 
 export default App;
