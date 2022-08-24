f3835f7d0b25cede92a3b5ae1facbafebed85831
SmartRenaldo <correctgavin@gmail.com>
2022-08-24 22:09:06 +0930

    learning logs

--- react-18-learning-logs/src/Components/Logs/Logs.js
+++ react-18-learning-logs/src/Components/Logs/Logs.js
@@ -2,30 +2,44 @@ import LogItem from './LogItem/LogItem';
 import './Logs.css';
 
 const Logs = () => {
-  return (
-    <div className='logs'>
-      <LogItem
-        date={new Date(new Date() - 15 * 24 * 60 * 60 * 1000)}
-        title={'React'}
-        time={'50'}
-      />
-      <LogItem
-        date={new Date(new Date() - 12 * 24 * 60 * 60 * 1000)}
-        title={'Angular'}
-        time={'40'}
-      />
-      <LogItem
-        date={new Date(new Date() - 9 * 24 * 60 * 60 * 1000)}
-        title={'TypeScript'}
-        time={'20'}
-      />
-      <LogItem
-        date={new Date(new Date() - 5 * 24 * 60 * 60 * 1000)}
-        title={'FPS'}
-        time={'40'}
-      />
-    </div>
-  );
+  const logsData = [
+    {
+      id: 0,
+      date: new Date(2022, 7, 15, 15, 30),
+      title: 'FPS',
+      time: '40',
+    },
+    {
+      id: 1,
+      date: new Date(2022, 7, 20, 19, 25),
+      title: 'TypeScript',
+      time: '20',
+    },
+    {
+      id: 2,
+      date: new Date(new Date() - 15 * 24 * 60 * 60 * 1000),
+      title: 'React',
+      time: '50',
+    },
+    {
+      id: 3,
+      date: new Date(new Date() - 12 * 24 * 60 * 60 * 1000),
+      title: 'Angular',
+      time: '40',
+    },
+  ];
+
+  const logItemDate = logsData.map((item) => (
+    <LogItem
+      key={item.id}
+      date={item.date}
+      title={item.title}
+      time={item.time}
+    />
+    // <LogItem key={item.id} {...item} />
+  ));
+
+  return <div className='logs'>{logItemDate}</div>;
 };
 
 export default Logs;
