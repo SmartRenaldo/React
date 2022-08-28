f9bfdf9ae951a893a44cebf733cce8803d111b05
Renaldo(Yiqi) Li <102466979+SmartRenaldo@users.noreply.github.com>
2022-08-28 20:15:36 +0930

    react 18 useRef

--- react-18-DOM-and-useRef/src/App.js
+++ react-18-DOM-and-useRef/src/App.js
@@ -1,15 +1,37 @@
-import './App.css';
+import {useRef, useState} from 'react';
+
+let tmp;
+let tmp2;
 
 const App = () => {
+  const refHeader = useRef();
+  const [count, setCount] = useState(1);
+  const refTmp = {current:null};
+  const refTmp2 = useRef();
+
+  console.log(tmp === refTmp);
+  console.log(tmp2 === refTmp2);
+
+  tmp = refTmp;
+  tmp2 = refTmp2;
+
   const clickHandler = () => {
-    const header = document.getElementById('header');
-    header.innerHTML = 'Hello World';
+    // console.log(refHeader);
+    refHeader.current.innerText = 'Hello World';
+  }
+
+  const addHandler = () => {
+    setCount(prevCount => prevCount + 1);
   }
 
   return (
     <div>
-      <h1 id='header'>Header</h1>
+      <h1 ref={refHeader}>Header</h1>
       <button onClick={clickHandler}>Press Here</button>
+      <br/>
+      <h1 onClick={addHandler}>Count Adder</h1>
+      <div className='display-6 fw-bold'>{count}</div>
+      <h1 onClick={changeRef}></h1>
     </div>
   );
 };
