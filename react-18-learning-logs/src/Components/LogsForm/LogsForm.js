1ea26232ba2682f3134c996feee26af08f7fcfc7
SmartRenaldo <correctgavin@gmail.com>
2022-09-21 22:13:25 +0930

    get form data

--- react-18-learning-logs/src/Components/LogsForm/LogsForm.js
+++ react-18-learning-logs/src/Components/LogsForm/LogsForm.js
@@ -1,25 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card/Card';
 import './LogsForm.css';
 
 const LogsForm = () => {
+  //save data in the form
+
+  const [inputDate, setInputDate] = useState('');
+  const [inputDesc, setInputDesc] = useState('');
+  const [inputTime, setInputTime] = useState('');
+
+  const dateChangeHandler = (e) => {
+    setInputDate(e.target.value);
+  };
+
+  const descChangeHandler = (e) => {
+    setInputDesc(e.target.value);
+  };
+
+  const timeChangeHandler = (e) => {
+    setInputTime(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const newLog = {
+      date: new Date(inputDate),
+      desc: inputDesc,
+      time: +inputTime,
+    };
+
+    setInputDate('');
+    setInputDesc('');
+    setInputTime('');
+
+    console.log(newLog);
+  };
+
   return (
     <Card className='logs-form'>
       <form>
         <div className='form-item'>
           <label htmlFor='date'>Date</label>
-          <input id='date' type='date' />
+          <input
+            onChange={dateChangeHandler}
+            value={inputDate}
+            id='date'
+            type='date'
+          />
         </div>
         <div className='form-item'>
           <label htmlFor='desc'>Content</label>
-          <input id='desc' type='text' />
+          <input
+            onChange={descChangeHandler}
+            value={inputDesc}
+            id='desc'
+            type='text'
+          />
         </div>
         <div className='form-item'>
           <label htmlFor='time'>Time</label>
-          <input id='time' type='number' />
+          <input
+            onChange={timeChangeHandler}
+            value={inputTime}
+            id='time'
+            type='number'
+          />
         </div>
         <div className='form-btn'>
-          <button>Add</button>
+          <button onClick={formSubmitHandler}>Add</button>
         </div>
       </form>
     </Card>
