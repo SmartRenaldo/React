91513f094c0e374b5d1a8f4422fc0e0cecde9872
SmartRenaldo <correctgavin@gmail.com>
2022-09-24 13:58:55 +0930

    inline style

--- react-18-create-app/src/App.js
+++ react-18-create-app/src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
+import './App.css';
 
 const App = () => {
-  return <div>App</div>;
+  const [isBlueBorder, setIsBlueBorder] = useState(true);
+
+  const pStyle = {
+    color: 'red',
+    backgroundColor: 'black',
+    border: isBlueBorder && 'blue solid 2px',
+  };
+
+  const clickHandler = () => {
+    setIsBlueBorder(!isBlueBorder);
+  };
+
+  return (
+    <div>
+      <p className={`p1 ${isBlueBorder ? '' : 'redBorder'}`} style={pStyle}>
+        biubiubiu~
+      </p>
+      <button onClick={clickHandler}>Click</button>
+    </div>
+  );
 };
 
 export default App;
